1cb38b5771a9d7810d759564c34606993279dec4
don-pkv <donpiyushkr@gmail.com>
2021-06-22 23:15:08 +0530

    1st complete Deploy

--- src/public/app/make-req.js
+++ src/public/app/make-req.js
@@ -1,8 +1,6 @@
-$('#write-btn').click(() => {
+$('#write-btn-2').click(() => {
       
       $('#content').load('/component/show-req.html')
-      $('.nav-item .active').removeClass('active')
-      $("[data-components='make-req']").addClass('active')
     
   },function showAlert(){
       alert('Request received successfully. If you want to provide any other specifications, use contact info given. Meanwhile check after 1-2 days for the link')
